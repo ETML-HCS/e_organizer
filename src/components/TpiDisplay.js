16551e04253828316b2ad04ebbef695b8b3b0604
HCS <helder.costa@eduvaud.ch>
2023-05-23 08:56:48 +0200

    Correction de bugs mineurs avec persistance des données restante
    Ce commit apporte des corrections pour résoudre certains bugs mineurs. Cependant, nous rencontrons toujours un problème important de persistance des données qui nécessite une attention supplémentaire. Les corrections effectuées visent à améliorer la stabilité du code existant et à préparer le terrain pour résoudre le problème persistant de persistance des données.
    
    Modifications apportées :
    - Correction de bugs mineurs qui ont été identifiés et résolus
    - Améliorations de la structure du code pour une meilleure maintenabilité
    - Réorganisation de certaines parties du code pour une meilleure lisibilité
    
    Problème persistant :
    Nous sommes toujours confrontés à un problème majeur de persistance des données. Malgré les corrections apportées, les données ne sont pas correctement conservées lors des interactions de l'utilisateur. Nous continuerons à travailler sur la résolution de ce problème pour assurer une expérience utilisateur cohérente et fiable.
    
    Prochaines étapes :
    - Examiner attentivement la gestion des états et des mises à jour de données dans les différentes parties du code
    - Effectuer des tests approfondis pour identifier les causes profondes du problème de persistance des données
    - Implémenter des solutions appropriées pour résoudre définitivement le problème
    
    Nous restons déterminés à résoudre ce problème de persistance des données et à fournir une application fiable et sans bugs. Votre soutien et vos commentaires sont précieux pour nous aider à progresser vers une résolution complète.

--- src/components/TpiDisplay.js
+++ src/components/TpiDisplay.js
@@ -1,22 +1,29 @@
-  import '../css/TpiDisplay.css';
+// #region imports
+import '../css/TpiDisplay.css';
 
-  import React, { useState, createContext, useContext, useEffect } from 'react';
-  import { DndProvider, useDrag, useDrop } from 'react-dnd';
-  import { HTML5Backend } from 'react-dnd-html5-backend';
+import React, { useState, useMemo } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-  import { format } from 'date-fns';
-  import frLocale from 'date-fns/locale/fr';
+import { format } from 'date-fns';
+import frLocale from 'date-fns/locale/fr';
+//#endregion
 
-  let tpiNumber = -1;
 
-  // Function to generate unique ID
-  const generateUniqueId = () => {
-    tpiNumber++;
-    return tpiNumber;
-  };
+let tpiNumber = -1;
 
-  const createNewTPI = () => {
-    return {
+// Function to generate unique ID
+
+const generateUniqueId = () => {
+  console.log(tpiNumber);
+  tpiNumber++;
+  return tpiNumber;
+};
+
+//#region Variables et constantes 
+const createNewTPI = () => {
+  return {
+    tpi: {
       id: generateUniqueId(),
       candidateFirstName: '',
       candidateLastName: '',
@@ -26,208 +33,280 @@
       expert2LastName: '',
       projectManagerFirstName: '',
       projectManagerLastName: '',
-    };
-  };
-
-  // Component to input information and drag a TPI card
-  const TpiCard = ({ tpiData, setTpiCardValues, roomIndex, timeSlotIndex }) => {
-
-    console.log("Dans la fonction TpiCard tpiData: ", setTpiCardValues);
-
-    const updateTpiCardValues = (roomIndex, timeSlotIndex, updatedFields) => {
-      setTpiCardValues((prevTpiCardValues) => {
-        const newTpiCardValues = [...prevTpiCardValues];
-        const updatedTpi = { ...newTpiCardValues[roomIndex][timeSlotIndex], ...updatedFields };
-        newTpiCardValues[roomIndex][timeSlotIndex] = updatedTpi;
-        return newTpiCardValues;
-      });
-    };
-    
-    const handleInputChange = (event, field) => {
-      const updatedFields = {
-        [field]: event.target.value,
-      };
-      updateTpiCardValues(roomIndex, timeSlotIndex, updatedFields);
-    };
-    
-    const inputTpi = (placeholder, value, field) => {
-      return (
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={value || ''}
-          onChange={(event) => handleInputChange(event, field)}
-        />
-      );
-    };
-
-    return (
-      <div className="tpiCard">
-        <div className="candidate">
-          {inputTpi("Candidate First Name", tpiData.candidateFirstName, 'candidateFirstName')}
-          {inputTpi("Candidate Last Name", tpiData.candidateLastName, 'candidateLastName')}
-        </div>
-        <div className="expert1">
-          {inputTpi("Exp 1 First Name", tpiData.expert1FirstName, 'expert1FirstName')}
-          {inputTpi("Exp 1 Last Name", tpiData.expert1LastName, 'expert1LastName')}
-        </div>
-        <div className="expert2">
-          {inputTpi("Exp 2 First Name", tpiData.expert2FirstName, 'expert2FirstName')}
-          {inputTpi("Exp 2 Last Name", tpiData.expert2LastName, 'expert2LastName')}
-        </div>
-        <div className="projectManager">
-          {inputTpi("PM First Name", tpiData.projectManagerFirstName, 'projectManagerFirstName')}
-          {inputTpi("PM Last Name", tpiData.projectManagerLastName, 'projectManagerLastName')}
-        </div>
-      </div>
-    );
-  };
-
-
-  const TpiSlot = ({ tpiData, setTpiCardValues, roomIndex, timeSlotIndex, onDrop }) => {
-
-    console.log(tpiData);
-
-    const [{ isOver }, drop] = useDrop(() => ({
-      accept: 'tpi',
-      drop: (item, monitor) => {
-        const tpiData = monitor.getItem().item;
-        onDrop(item, roomIndex, timeSlotIndex, tpiData);
-      },
-      collect: (monitor) => ({
-        isOver: monitor.isOver(),
-      }),
-    }));
-
-    return (
-      <div ref={drop} className={`tpiSlot${isOver ? 'active' : ''}`}>
-        <TpiCard
-          roomIndex={roomIndex}
-          timeSlotIndex={timeSlotIndex}
-          tpiData={tpiData}
-          setTpiCardValues={setTpiCardValues}
-        />
-      </div>
-    );
-  };
-
-  // Main function that sends the planning and all the necessary functions for editing TPIs.
-  const TpiDisplay = ({ timeSlots, primaryColor, candidateColor, backgroundColor }) => {
-
-    // il s'agit du nombre de salle affiché et traité à l'écran
-    const [roomCount, setRoomCount] = useState(0);
-
-    let tpiList = [];
-
-    for (let j = 0; j < roomCount + 1; j++) {
-      tpiList[j] = [];
-      for (let i = 0; i < timeSlots.length; i++) {
-        const newTpi = createNewTPI();
-        tpiList[j][i] = newTpi;
-      }
     }
+  };
+};
 
-    // props contient tous les données des différents tpi (idée [Salle][Creneau])
-    const [tpiCardValues, setTpiCardValues] = useState(tpiList);
 
-    // personnalisation du thème
-    const customStyles = {
-      '--primary-color': primaryColor,
-      '--candidate-color': candidateColor,
-      '--content-color': backgroundColor,
-    };
+const InitListTPI = (nbrTimeSlots) => {
+  let tpiList = useMemo(() => {
+    const tempList = [];
+    for (let i = 0; i < nbrTimeSlots; i++) {
+      let newTpi = createNewTPI();
+      tempList[i] = newTpi;
+    }
+    return tempList;
+  }, [nbrTimeSlots]);
 
-    // traitement de l'affichage des créneaux (exemple 09h50)
-    const displayTime = (time) => {
-      return time.slice(0, 5);
-    };
+  return tpiList;
+};
 
-    // traitement de l'affichage des dates (exemple Samedi, le 5 avril)
-    const capitalize = (str) => {
-      const firstSpaceIndex = str.indexOf(' ');
+//#endregion
 
-      if (firstSpaceIndex !== -1) {
-        str = str.replace(' ', ', the ');
-      }
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    };
 
-    // fonction lorsqu'un tpi est déplacé
-    const handleDrop = (item, roomIndex, timeSlotIndex, tpiData) => {
-      console.log("valeur de item: ", item, " val d'index : ", roomIndex, " val tpidata: ", tpiData);
-      console.log("Updated tpiCardValues:", tpiCardValues);
-    };
+//#region Fonction TpiCard
+const TpiCard = ({ tpiData, setTpiCardValues, index, onDrag }) => {
 
-    // fonction qui ajout une salle
-    const handleAddRoom = () => {
-      setRoomCount(roomCount + 1);
-    };
-
-    // fonction qui sauvegarde tout les données
-    const handleSaveRoom = () => { };
-
-    // affichage HTML + CSS
-    return (
-      <DndProvider backend={HTML5Backend}>
-        <div>
-          <table className="content-table">
-            <tbody>
-              <tr>
-                {Array.from({ length: roomCount + 1 }).map((_, roomIndex) => (
-                  <td className="content" style={customStyles} key={roomIndex}>
-                    <div className="date">
-                      {capitalize(
-                        format(new Date("2023-06-17"), 'EEEE d MMMM', {
-                          locale: frLocale,
-                        })
-                      )}
-                    </div>
-                    <div className="timeSlots">
-                      <div className="whiteRoom"></div>
-                      <div className="room">A23</div>
-                      {/* boucle des créneaux, par défaut 8 */}
-                      {
-                        timeSlots.map((timeSlot, timeSlotIndex) => {
-
-                          console.log("image de tpicardvalues : ", tpiCardValues);
-
-                          return (
-                            <React.Fragment key={timeSlotIndex}>
-                              <div className="schedule">
-                                <div className="start">{displayTime(timeSlot.start)}</div>
-                                <div className="end">{displayTime(timeSlot.end)}</div>
-                              </div>
-
-                              <div className="zone2"></div>
-                              {/* Emplacement du tpiData */}
-                              <div className="tpis">
-                                <TpiSlot
-                                  roomIndex={roomIndex}
-                                  timeSlotIndex={timeSlotIndex}
-                                  onDrop={handleDrop}
-                                  tpiData={tpiCardValues[roomIndex][timeSlotIndex]}
-                                  setTpiCardValues={setTpiCardValues}
-                                />
-                              </div>
-                              <div className="breakRow"></div>
-                            </React.Fragment>
-                          );
-                        })}
-
-                    </div>
-                  </td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
-
-          <div>
-            <input type="button" className="addButton" title="Add a room" value="+" onClick={handleAddRoom} />
-            <input type="button" className="saveButton" title="Save the data" value="Save" onClick={handleSaveRoom} />
-          </div>
-        </div>
-      </DndProvider>
-    );
+  //#region  callback change input text temps réel 
+  
+  const updateTpiCardValues = (index, updatedFields) => {  
+    setTpiCardValues((prevTpiCardValues) => {
+      const newTpiCardValues = [...prevTpiCardValues];
+      const updatedTpi = {
+        ...newTpiCardValues[index],
+        ...updatedFields
+      };
+      newTpiCardValues[index] = updatedTpi;
+      
+      return newTpiCardValues;
+    });
   };
 
-  export default TpiDisplay;
+  const handleInputChange = (event, field) => {
+    const updatedFields = {
+      [field]: event.target.value,
+    };
+    updateTpiCardValues(index, updatedFields);
+  };
+  //#endregion
+
+  //#region Construction Input <Text>
+  const inputTpi = (placeholder, value, field) => {
+    return (
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value || ''}
+        onChange={(event) => handleInputChange(event, field)}
+      />
+    );
+  };
+  //#endregion
+
+  //#region  Callback drag
+  const [{ isDragging }, drag] = useDrag( () => ({
+      type: 'tpi',
+      item: { tpi: tpiData },
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+      end: (tpi, monitor) => {
+
+        if (monitor.didDrop()) {
+          // Appel de la fonction onDrag avec l'index et les données de l'élément
+          console.log("Callbag drag : ",index," val :",tpi);
+          onDrag(index, tpi);
+        }
+      },
+    }),
+  );
+  //#endregion
+
+  //#region retour d'un rendu HTML+ClasseName d'un TPI  
+  return (
+    <div ref={drag} className={`tpiCard${isDragging ? 'dragging' : ''}`}>
+      <div className="candidate">
+        {inputTpi("Candidate First Name", tpiData.candidateFirstName, 'candidateFirstName')}
+        {inputTpi("Candidate Last Name", tpiData.candidateLastName, 'candidateLastName')}
+      </div>
+      <div className="expert1">
+        {inputTpi("Exp 1 First Name", tpiData.expert1FirstName, 'expert1FirstName')}
+        {inputTpi("Exp 1 Last Name", tpiData.expert1LastName, 'expert1LastName')}
+      </div>
+      <div className="expert2">
+        {inputTpi("Exp 2 First Name", tpiData.expert2FirstName, 'expert2FirstName')}
+        {inputTpi("Exp 2 Last Name", tpiData.expert2LastName, 'expert2LastName')}
+      </div>
+      <div className="projectManager">
+        {inputTpi("PM First Name", tpiData.projectManagerFirstName, 'projectManagerFirstName')}
+        {inputTpi("PM Last Name", tpiData.projectManagerLastName, 'projectManagerLastName')}
+      </div>
+    </div>
+  );
+  //#endregion
+};
+//#endregion
+
+const TpiSlot = ({ tpiData, setTpiCardValues, index, onDrop, onDrag }) => {
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'tpi',
+    drop: () => {
+      console.log("Callback Drop ",index," val de tpi : ",tpiData);
+      onDrop(index, tpiData);
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+  }));
+
+  return (
+    <div ref={drop} className={`tpiSlot${isOver ? 'active' : ''}`}>
+      <TpiCard
+        index={index}
+        tpiData={tpiData}
+        onDrag={onDrag}
+        setTpiCardValues={setTpiCardValues}
+      />
+    </div>
+  );
+};
+
+// Main function that sends the planning and all the necessary functions for editing TPIs.
+const TpiDisplay = ({ timeSlots, primaryColor, candidateColor, backgroundColor }) => {
+
+  // variables d'état pour l'échange entre deux tpi 
+  const [tpiDataDropping, setTpiDataDropping] = useState(null);
+  const [indexTpiDropping, setindexTpiDropping ] = useState(null);
+
+  // il s'agit du nombre de salle affiché et traité à l'écran
+  const [roomCount, setRoomCount] = useState(1);
+
+  // props contient tous les données des différents tpi
+  const [tpiCardValues, setTpiCardValues] = useState(InitListTPI(timeSlots.length));
+
+
+  // personnalisation du thème
+  const customStyles = {
+    '--primary-color': primaryColor,
+    '--candidate-color': candidateColor,
+    '--content-color': backgroundColor,
+  };
+
+  // traitement de l'affichage des créneaux (exemple 09h50)
+  const displayTime = (time) => {
+    return time.slice(0, 5);
+  };
+
+  // traitement de l'affichage des dates (exemple Samedi, le 5 avril)
+  const capitalize = (str) => {
+    const firstSpaceIndex = str.indexOf(' ');
+
+    if (firstSpaceIndex !== -1) {
+      str = str.replace(' ', ', the ');
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  // Fonction qui mémorise les données lorsque l'utilisateur attrappe un TPI 
+  const handleDrag = (indexTpiDragging, tpiDataDragging) => {
+
+    tpiCardValues.map((tpicard, i) => {
+      console.log(`Valeur du champ tpicard ${i}:`, tpicard);
+      return null; // Retourne null pour éviter un avertissement sur la clé de l'élément dans la boucle map
+    });
+    
+
+    // copie des carte 
+    let updatedTpiCardValues = [...tpiCardValues];
+
+    console.log("index dropping : ", indexTpiDropping);
+
+    updatedTpiCardValues[indexTpiDropping] = tpiDataDragging;
+
+    setTpiCardValues(updatedTpiCardValues);
+
+    console.log("Updated tpiCardValues after swap:", updatedTpiCardValues);
+    console.log("index dst : ", indexTpiDragging, " src : ", indexTpiDropping, " tpiDest :", tpiDataDropping, "tpiSrc :", tpiDataDragging);
+ 
+    //  // Réinitialiser les valeurs après le drop
+    setTpiDataDropping(null);
+    setindexTpiDropping(null);
+  };
+  
+
+  // fonction lorsqu'un tpi est déplacé Drag and drop 
+  const handleDrop = (indexTpiDropping, tpiDataDropping) => {
+
+    setTpiDataDropping(tpiDataDropping);
+    setindexTpiDropping(indexTpiDropping);
+
+    console.log("Valeur à jour : ",tpiDataDropping, " index : ",indexTpiDropping);
+
+  };
+  
+
+  // fonction qui ajout une salle
+  const handleAddRoom = () => {
+    setRoomCount(roomCount + 1);
+    
+    const tmp = [...tpiCardValues, ...InitListTPI(roomCount)];
+    setTpiCardValues(tmp);
+  };
+
+  // fonction qui sauvegarde tout les données
+  const handleSaveRoom = () => { };
+
+  // affichage HTML + CSS
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div>
+        <table className="content-table">
+          <tbody>
+            <tr>
+              {Array.from({ length: roomCount }).map((_, roomIndex) => (
+                <td className="content" style={customStyles} key={roomIndex}>
+                  <div className="date">
+                    {capitalize(
+                      format(new Date("2023-06-17"), 'EEEE d MMMM', {
+                        locale: frLocale,
+                      })
+                    )}
+                  </div>
+                  <div className="timeSlots">
+                    <div className="whiteRoom"></div>
+                    <div className="room">A23</div>
+                    {/* boucle des créneaux, par défaut 8 */}
+                    {
+                      timeSlots.map((timeSlot, timeSlotIndex) => {
+
+                        console.log("image de tpicardvalues : ", tpiCardValues);
+
+                        return (
+                          <React.Fragment key={(roomIndex * 8) + timeSlotIndex}>
+                            <div className="schedule">
+                              <div className="start">{displayTime(timeSlot.start)}</div>
+                              <div className="end">{displayTime(timeSlot.end)}</div>
+                            </div>
+
+                            <div className="zone2"></div>
+                            {/* Emplacement du tpiData */}
+                            <div className="tpis">
+                              <TpiSlot
+                                index={(roomIndex * 8) + timeSlotIndex}
+                                onDrop={handleDrop}
+                                onDrag={handleDrag}
+                                tpiData={tpiCardValues[(roomIndex * 8) + timeSlotIndex]}
+                                setTpiCardValues={setTpiCardValues}
+                              />
+                            </div>
+                            <div className="breakRow"></div>
+                          </React.Fragment>
+                        );
+                      })}
+
+                  </div>
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+
+        <div>
+          <input type="button" className="addButton" title="Add a room" value="+" onClick={handleAddRoom} />
+          <input type="button" className="saveButton" title="Save the data" value="Save" onClick={handleSaveRoom} />
+        </div>
+      </div>
+    </DndProvider>
+  );
+}; export default TpiDisplay;
